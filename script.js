1e81b7378b4980877fc66e863a02daffff15f01b
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-28 19:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const Form=document.querySelector("#form");
+Form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	const Age=document.querySelector('#age').value;
+	const Name=document.querySelector('#name').value.trim();
+
+	if(!Age || !Name){
+		alert("Please Enter valid details.");
+		return;
+	}
+
+	new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(Age>18){
+				resolve(`Welcome, ${Name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	}).then((message)=>alert(message)).catch((message)=>alert(message));
+
+	
+})
